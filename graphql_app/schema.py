733d923d1d1518765e5b7e1280bb91d12a1bca02
Vitalii Mytenko <vetalmit@gmail.com>
2022-04-09 22:37:11 +0300

    Add django-filters to queries.

--- graphql_app/schema.py
+++ graphql_app/schema.py
@@ -1,35 +1,77 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
-from .models import Student, Subject, Teacher
+from graphql_app.models import Student, Subject, Teacher
 
 
 class TeacherType(DjangoObjectType):
     class Meta:
         model = Teacher
         fields = ("id", "full_name", "subject")
+        filter_fields = {
+            "id": ["exact"],
+            "full_name": ["exact", "icontains", "istartswith"],
+        }
+        interfaces = (graphene.relay.Node,)
 
 
 class SubjectType(DjangoObjectType):
     class Meta:
         model = Subject
         fields = ("id", "name")
+        filter_fields = {"name": ["exact", "icontains", "istartswith"]}
+        interfaces = (graphene.relay.Node,)
 
 
 class StudentType(DjangoObjectType):
     class Meta:
         model = Student
         fields = ("id", "full_name", "subject")
+        filter_fields = {
+            "id": ["exact"],
+            "full_name": ["exact", "icontains", "istartswith"],
+        }
+        interfaces = (graphene.relay.Node,)
+
+
+class TeacherConnection(graphene.relay.Connection):
+    """Pagination and slicing for Teachers list."""
+
+    class Meta:
+        node = TeacherType
+
+
+class StudentConnection(graphene.relay.Connection):
+    """Pagination and slicing for Students list."""
+
+    class Meta:
+        node = StudentType
+
+
+class SubjectConnection(graphene.relay.Connection):
+    """Pagination and slicing for Subjects list."""
+
+    class Meta:
+        node = SubjectType
 
 
 class Query(graphene.ObjectType):
-    teacher = graphene.Field(TeacherType, id=graphene.Int())
-    subject = graphene.Field(SubjectType, id=graphene.Int())
-    student = graphene.Field(StudentType, id=graphene.Int())
+    teacher = graphene.relay.Node.Field(TeacherType)
+    teachers = DjangoFilterConnectionField(TeacherType)
 
-    teachers = graphene.List(TeacherType)
-    subjects = graphene.List(SubjectType)
-    students = graphene.List(StudentType)
+    subject = graphene.relay.Node.Field(SubjectType)
+    subjects = DjangoFilterConnectionField(SubjectType)
+
+    student = graphene.relay.Node.Field(StudentType)
+    students = DjangoFilterConnectionField(StudentType)
+
+    # teacher = graphene.Field(TeacherType, id=graphene.Int())
+    # subject = graphene.Field(SubjectType, id=graphene.Int())
+    # student = graphene.Field(StudentType, id=graphene.Int())
+    # teachers = graphene.List(TeacherType)
+    # subjects = graphene.List(SubjectType)
+    # students = graphene.List(StudentType)
 
     def resolve_teacher(self, info, **kwargs):
         num = kwargs.get("id", None)
@@ -52,13 +94,13 @@ class Query(graphene.ObjectType):
         except Student.DoesNotExist:
             return None
 
-    def resolve_teachers(self, info):
+    def resolve_teachers(self, info, **kwargs):
         return Teacher.objects.all()
 
-    def resolve_subjects(self, info):
+    def resolve_subjects(self, info, **kwargs):
         return Subject.objects.all()
 
-    def resolve_students(self, info):
+    def resolve_students(self, info, **kwargs):
         return Student.objects.all()
 
 
---
+++ schema.py
@@ -0,0 +1,18 @@
+import graphene
+
+from graphql_app import schema
+
+# from graph_app_two import mutations
+
+
+class Query(schema.Query, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+
+# class Mutation(mutations.Mutation, graphene.ObjectType):
+#     pass
+
+
+scheme = graphene.Schema(query=Query)
