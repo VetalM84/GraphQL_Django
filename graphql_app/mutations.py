74c30e03cdb271db5791a031e16c7e9dea3fdfc5
Vitalii Mytenko <vetalmit@gmail.com>
2022-04-10 21:17:50 +0300

    Add Student mutations

--- graphql_app/mutations.py
+++ graphql_app/mutations.py
@@ -6,6 +6,68 @@ from graphql_app.models import Student, Subject, Teacher
 from graphql_app.types import StudentType, SubjectType, TeacherType
 
 
+class StudentInput(graphene.InputObjectType):
+    full_name = graphene.String()
+    subject = graphene.List(graphene.Int)
+    avg_grade = graphene.Float()
+    id = graphene.ID()
+
+
+class CreateStudent(graphene.Mutation):
+    class Arguments:
+        input = StudentInput(required=True)
+
+    ok = graphene.Boolean()
+    student = graphene.Field(StudentType)
+
+    @classmethod
+    def mutate(cls, root, info, input=None):
+        ok = True
+        student_instance = Student.objects.create(full_name=input.full_name, avg_grade=input.avg_grade)
+        student_instance.subject.add(*input.subject)
+        return cls(ok=ok, student=student_instance)
+
+
+class UpdateStudent(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = StudentInput(required=True)
+
+    ok = graphene.Boolean()
+    student = graphene.Field(StudentType)
+
+    @classmethod
+    def mutate(cls, root, info, id, input=None):
+        ok = False
+        try:
+            student_instance = Student.objects.get(pk=id)
+        except Student.DoesNotExist:
+            return cls(ok=ok, teacher=None)
+
+        ok = True
+        student_instance.full_name = input.full_name
+        student_instance.avg_grade = input.avg_grade
+        student_instance.subject.add(*input.subject)
+        student_instance.save()
+        return cls(ok=ok, student=student_instance)
+
+
+class DeleteStudent(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    ok = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        try:
+            student_instance = Student.objects.get(pk=id)
+            student_instance.delete()
+            return cls(ok=True)
+        except Student.DoesNotExist:
+            return cls(ok=True)
+
+
 class TeacherInput(graphene.InputObjectType):
     full_name = graphene.String()
     subject = graphene.List(graphene.Int)
@@ -70,3 +132,7 @@ class Mutation(graphene.ObjectType):
     create_teacher = CreateTeacher.Field()
     update_teacher = UpdateTeacher.Field()
     delete_teacher = DeleteTeacher.Field()
+
+    create_student = CreateStudent.Field()
+    update_student = UpdateStudent.Field()
+    delete_student = DeleteStudent.Field()
